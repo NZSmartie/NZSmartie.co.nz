37dd56be96897265552371aa5f8c2a779103d7e0
NZSmartie <nzsmartie@gmail.com>
2018-05-30 00:14:46 +1200

    Update build scripts

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from scss import Compiler as ScssCompiler
 from pathlib import Path
@@ -14,10 +14,41 @@ config = {
     'output': 'public/'
 }
 
+def clean():
+    output = Path(config['output'])
+
+    depth = 0
+    parents = []
+    for file in output.glob('**/*'):
+        if '.git' in str(file):
+            continue
+
+        if file.is_file():
+            print("Deleting {0}".format(file))
+            file.unlink()
+        else:
+            parents.append(file)
+
+        # print([p.name for p in file.relative_to(output).parents][:-1])
+        # unlink()
+
+    parents.reverse()
+    for dir in parents:
+        print("Deleting {0}".format(dir))
+        dir.rmdir()
+
+    # for dir in output.
+    #     rmdir()
+
 def generate():
     # Generate css files using pyScss
     root = Path(config['root'])
     output = Path(config['output'])
+
+    # Copy CNAME
+    cname = 'CNAME'
+    (output / cname).write_text((root / cname).read_text())
+
 
     scss = ScssCompiler(root)
 
